e1d03320ed60c212156b0340e579c433e74223e2
parn_kas <pkassraie@gmail.com>
2018-07-17 16:38:39 +0200

    Generating Volcanos with FGSM

--- massRun.py
+++ massRun.py
@@ -1,9 +1,15 @@
-#from visualization.vanilla_backprop import runVanillaBP
+
 from visualization import *
-runDeepDream()
-#runGradCam()
-#runGGradCam()
-#runGBackProp()
-#runInvRep()
-#runsmoothGrad()
-#runVanillaBP()
+
+attacks = ['FGSM','PGD','Boundary','SinglePixel','DeepFool']
+networks = ['VGG19','AlexNet']
+target_example = range(0, 3) #its 4 really
+
+#for i in target_example:
+    #runDeepDream(target_example=i)
+#runGradCam(target_example=3,choose_network='VGG19')
+runGGradCam(target_example=3,choose_network='VGG19')
+    #runGBackProp(target_example=i)
+    #runInvRep(target_example=i)
+    #runsmoothGrad(target_example=i)
+    #runVanillaBP(target_example=i)
