3bc2c9b9e507ce0dcf20c42c08680bc0d399b4bf
parn_kas <pkassraie@gmail.com>
2018-08-07 16:45:35 +0200

    GradCam Bug fix + adding ResNet50

--- visualization/gradcam.py
+++ visualization/gradcam.py
@@ -22,27 +22,48 @@ class CamExtractor():
     def save_gradient(self, grad):
         self.gradients = grad
 
-    def forward_pass_on_convolutions(self, x):
+    def forward_pass_on_convolutions(self, x, network):
         """
             Does a forward pass on convolutions, hooks the function at given layer
         """
         conv_output = None
-        for module_pos, module in self.model.features._modules.items():
-            x = module(x)  # Forward
-            if int(module_pos) == self.target_layer:
-                x.register_hook(self.save_gradient)
-                conv_output = x  # Save the convolution output on that layer
+
+        if network =="ResNet50":
+            i = 0
+            for module in list(self.model.children())[:-1]:
+                i += 1
+                x = module(x)
+                print("i:",i,"- target:",self.target_layer)
+                if i == self.target_layer:
+                    x.register_hook(self.save_gradient)
+                    conv_output = x
+                    print("i'm here")
+
+        else:
+            for module_pos, module in self.model.features._modules.items():
+                # print("module_pos: ",module_pos," - Module:" , module)
+                x = module(x)  # Forward
+                if int(module_pos) == self.target_layer:
+                    x.register_hook(self.save_gradient)
+                    conv_output = x  # Save the convolution output on that layer
+
         return conv_output, x
 
-    def forward_pass(self, x):
+    def forward_pass(self, x,network):
         """
             Does a full forward pass on the model
         """
         # Forward pass on the convolutions
-        conv_output, x = self.forward_pass_on_convolutions(x)
-        x = x.view(x.size(0), -1)  # Flatten
+        conv_output, x = self.forward_pass_on_convolutions(x,network)
+
         # Forward pass on the classifier
-        x = self.model.classifier(x)
+        x = x.view(x.size(0), -1)  # Flatten
+        if network == "ResNet50":
+            module  = list(self.model.children())[-1]
+            x = module(x)
+
+        else:
+            x = self.model.classifier(x)
         return conv_output, x
 
 
@@ -56,29 +77,42 @@ class GradCam():
         # Define extractor
         self.extractor = CamExtractor(self.model, target_layer)
 
-    def generate_cam(self, input_image, target_class=None):
+    def generate_cam(self, input_image, network, target_class=None):
         # Full forward pass
         # conv_output is the output of convolutions at specified layer
         # model_output is the final output of the model (1, 1000)
-        conv_output, model_output = self.extractor.forward_pass(input_image)
+        conv_output, model_output = self.extractor.forward_pass(input_image,network)
         if target_class is None:
             target_class = np.argmax(model_output.data.numpy())
         # Target for backprop
         one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
         one_hot_output[0][target_class] = 1
+
         # Zero grads
-        self.model.features.zero_grad()
-        self.model.classifier.zero_grad()
+        if network == "ResNet50":
+            for module in list(self.model.children())[:-1]:
+                module.zero_grad()
+            module = list(self.model.children())[-1]
+            module.zero_grad()
+        else:
+            self.model.features.zero_grad()
+            self.model.classifier.zero_grad()
+
         # Backward pass with specified target
         model_output.backward(gradient=one_hot_output, retain_graph=True)
+
         # Get hooked gradients
         guided_gradients = self.extractor.gradients.data.numpy()[0]
+
         # Get convolution outputs
         target = conv_output.data.numpy()[0]
+
         # Get weights from gradients
         weights = np.mean(guided_gradients, axis=(1, 2))  # Take averages for each gradient
+
         # Create empty numpy array for cam
         cam = np.ones(target.shape[1:], dtype=np.float32)
+
         # Multiply each weight with its conv output and then, sum
         for i, w in enumerate(weights):
             cam += w * target[i, :, :]
@@ -100,9 +134,16 @@ def runGradCam(choose_network = 'AlexNet',
         get_params(target_example,choose_network,isTrained)
 
     # Grad cam
-    grad_cam = GradCam(pretrained_model, target_layer=11)
+    if choose_network == "ResNet50":
+        grad_cam = GradCam(pretrained_model, target_layer=8)
+    elif choose_network == "AlexNet":
+        grad_cam = GradCam(pretrained_model, target_layer=11)
+    elif choose_network == "VGG19":
+        grad_cam = GradCam(pretrained_model, target_layer=35)
+
+
     # Generate cam mask
-    cam = grad_cam.generate_cam(prep_img, target_class)
+    cam = grad_cam.generate_cam(prep_img, choose_network,target_class)
     # Save mask
     gray,color, result = save_class_activation_on_image(original_image, cam, file_name_to_export)
 
@@ -111,16 +152,14 @@ def runGradCam(choose_network = 'AlexNet',
         # Adversary:
 
     attack1 = attack(attack_type,pretrained_model,original_image,file_name_to_export,target_class)
-    adversarial,advers_class,orig_pred,adver_pred ,diff = attack1.getstuff()
+    adversarialpic,adversarial,advers_class,orig_pred,adver_pred ,diff = attack1.getstuff()
 
     orig_labs,orig_vals = prediction_reader(orig_pred,10)
     adver_labs,adver_vals = prediction_reader(adver_pred,10)
     indices = np.arange(len(orig_labs))
 
-    # Grad cam
-    grad_cam = GradCam(pretrained_model, target_layer=11)
     # Generate cam mask
-    cam = grad_cam.generate_cam(adversarial,advers_class)
+    cam = grad_cam.generate_cam(adversarial,choose_network,advers_class)
     # Save mask
     gray2,color2, result2 = save_class_activation_on_image(original_image, cam, 'Adversary_'+file_name_to_export)
     print('Adversary Grad cam completed')
@@ -149,7 +188,7 @@ def runGradCam(choose_network = 'AlexNet',
     ax9.set_xticks(indices)
     ax9.set_xticklabels(orig_labs,rotation = 45,ha="right")
 
-    adversarial = np.uint8(attack1.getadvers())
+    adversarial = np.uint8(adversarialpic)
     ax12 = fig.add_subplot(2,5,6)
     ax12.imshow(cv2.cvtColor(adversarial, cv2.COLOR_BGR2RGB))
     ax12.set_title('Adversary Image(SSIM = '+str(diff)+')')
